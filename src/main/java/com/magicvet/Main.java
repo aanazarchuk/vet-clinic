5f112b351a5145c38888daa0636a733f17d47105
Angelina <93387930+aanazarchuk@users.noreply.github.com>
2024-04-18 14:00:24 +0200

    REQ-01: Base authentication

--- src/main/java/com/magicvet/Main.java
+++ src/main/java/com/magicvet/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    static String PASSWORD = "default";
+    static String PASSWORD = "defaul";
     static Scanner scanner= new Scanner(System.in);
 
     public static void main(String[] args) {
