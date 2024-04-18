d1346d52bab5a3e5199f41e2da417062d7c49e34
Angelina <93387930+aanazarchuk@users.noreply.github.com>
2024-04-18 13:59:00 +0200

    New features

---
+++ src/main/java/com/magicvet/Main.java
@@ -0,0 +1,29 @@
+package main.java.com.magicvet;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    static String PASSWORD = "default";
+    static Scanner scanner= new Scanner(System.in);
+
+    public static void main(String[] args) {
+      run();
+    }
+
+    static void run (){
+        boolean accepted = false;
+        for(int i = 0; i<3; i++){
+            System.out.print("Password: ");
+            String input = scanner.nextLine();
+
+            if (PASSWORD.equals(input)) {accepted =true; break;}
+            else {
+                System.out.println("Access denied.Please check your password.");
+            }
+        }
+        System.out.println(accepted ? "Welcome to the Magic-vet" : "Application has been blocked");
+    }
+}
