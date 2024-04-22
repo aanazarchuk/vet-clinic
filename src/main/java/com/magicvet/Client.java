47e73a59318466db2aac68bf8cfc8e5a50f9478a
Angelina <93387930+aanazarchuk@users.noreply.github.com>
2024-04-22 19:57:48 +0200

    REQ-01: Base authentication

---
+++ src/main/java/com/magicvet/Client.java
@@ -0,0 +1,7 @@
+package main.java.com.magicvet;
+
+public class Client {
+    String name;
+    String lastname;
+    String mail;
+}
--- src/main/java/com/magicvet/Main.java
+++ src/main/java/com/magicvet/Main.java
@@ -1,29 +1,76 @@
 package main.java.com.magicvet;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     static String PASSWORD = "default";
-    static Scanner scanner= new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);
+    static String Email_Pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
 
     public static void main(String[] args) {
-      run();
+        run();
     }
 
-    static void run (){
-        boolean accepted = false;
-        for(int i = 0; i<3; i++){
-            System.out.print("Password: ");
+    static void run() {
+     if (auth()){
+       registerNewClient();
+     }
+    }
+
+    static void registerNewClient() {
+        System.out.print("Please provide a client details");
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+
+        if (isEmailValid(email)) {
+            Client client =buildClient(email);
+            System.out.println("New client " + client.name+ " " + client.lastname+" ("+client.mail + ")");
+        }else{
+            System.out.println("Provide email is valid");
+        }
+    }
+
+     static Client buildClient(String email) {
+        Client client = new Client();
+        client.mail = email;
+
+         System.out.print("First name: ");
+         client.name = scanner.nextLine();
+
+         System.out.print("Last name: ");
+         client.lastname=scanner.nextLine();
+
+        return client;
+    }
+
+     static boolean isEmailValid(String email) {
+        Pattern pattern = Pattern.compile(Email_Pattern);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    static boolean auth(){
+        boolean accepted = true;
+        for(int i =0; i<3; i++){
+            System.out.println("Password: ");
             String input = scanner.nextLine();
 
-            if (PASSWORD.equals(input)) {accepted =true; break;}
-            else {
-                System.out.println("Access denied.Please check your password.");
+            if(PASSWORD.equals(input)){
+                accepted = true;
+                break;
+            }else {
+                System.out.println("Access denided.Please check your password");
             }
         }
-        System.out.println(accepted ? "Welcome to the Magic-vet" : "Application has been blocked");
+        System.out.println(accepted ? "Welcome to the Magic Vet!" : "Application has been blocked.");
+
+        return accepted;
+
     }
+
 }
